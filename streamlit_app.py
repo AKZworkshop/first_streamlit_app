df538f63d2a58b2a71776b9d0fb7a3e4252f4352
AKZworkshop <148958569+AKZworkshop@users.noreply.github.com>
2023-10-25 14:41:53 +0200

    Update streamlit_app.py
    cos

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# Utwórz listę etykiet na podstawie kolumny 'Fruit'
-fruit_labels = list(my_fruit_list['Fruit'])
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", fruit_labels)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list['Fruit'])))
 
 # Display the table on the page
 streamlit.dataframe(my_fruit_list)
